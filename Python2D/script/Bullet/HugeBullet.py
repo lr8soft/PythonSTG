cfa7242d47856dbed305a0892876f1f5b28c997b
LT_lrsoft <lrsoft@hotmail.com>
2019-10-06 15:49:45 +0800

    v0.20x

---
+++ Python2D/script/Bullet/HugeBullet.py
@@ -0,0 +1,17 @@
+from script.Bullet.Bullet import Bullet
+class CircleBullet(Bullet):
+    def __init__(self, position = [0.0,0.0,0.0],velocity = 0.01, angle = 0.0):
+        super().__init__(position, velocity, angle)
+        self.divideInfo = [8,1,6,1]
+        self.scaleSize = [0.055,0.055,0.055]
+        self.bulletType = "circleBullet"
+
+    def _cpp_getInitRenderInfo(self):
+        return (self.bulletType, tuple(self.divideInfo), tuple(self.scaleSize))
+
+
+
+
+
+
+
